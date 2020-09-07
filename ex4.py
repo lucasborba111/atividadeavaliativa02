80bd216d3373a0d9962b3abedcf452920350922a
lucasborba111 <lucasdbb.12@gmail.com>
2020-09-07 15:07:01 -0300

    feat exercicio4

---
+++ ex4.py
@@ -0,0 +1,11 @@
+filme = ["harry", "spider", "toy"]
+jogo = ["gta", "mine", "pes"]
+livro = ["clean", "biblia", "saci"]
+esporte = ["futebol", "ciclismo", "natação"]
+
+lista = [filme+jogo+livro+esporte]
+
+print(livro[1])
+jogo.insert(3,"matematica")
+jogo.insert(4, "geografia")
+print(jogo)
